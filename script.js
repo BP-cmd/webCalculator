db74a83a0be431772f776fe81f5912bc893f56a9
BP-cmd <bhaswadip@gmail.com>
2025-04-06 01:47:56 +0530

    fix bugs, add toggle, reset and clear functionality

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ let display = document.querySelector(".display");
 let oppKeys = document.querySelectorAll(".opp");
 let equals = document.querySelector(".equals");
 let reset = document.querySelector(".reset");
+let clear = document.querySelector(".clear");
+let plusMinus = document.querySelector(".toggle");
 let num1, num2, operator;
 let newEntry = true;
+
+
 
 digits.forEach(digit => digit.addEventListener("click", () => updateDisplay(digit.innerText)));
 oppKeys.forEach(key=>{
@@ -15,10 +19,18 @@ oppKeys.forEach(key=>{
         operator=key.innerText;    
         num1 = Number(display.innerText);
         newEntry = true;
+        key.classList.toggle("selected");
     })
 });
 equals.addEventListener("click", operate);
 reset.addEventListener("click", resetCal);
+clear.addEventListener("click", ()=>{
+    if(!newEntry){
+        display.innerText = display.innerText.slice(0, -1);
+    }
+})
+plusMinus.addEventListener("click", togglePlusMinus);
+
 
 function updateDisplay(str){
     if(newEntry) {
@@ -35,6 +47,7 @@ function operate(){
     newEntry = true;
     updateDisplay(calculate(num1, num2, operator));
     operator = undefined;
+    deselectOpps();
 }
 
 function calculate (n1, n2, operation){
@@ -50,4 +63,16 @@ function resetCal(){
     display.innerText = "0";
     newEntry = true;
     operator = undefined;
+}
+
+function deselectOpps(){
+    oppKeys.forEach(key=>{
+        if(key.classList.contains("selected")){
+            key.classList.remove("selected");
+        }
+    })
+}
+
+function togglePlusMinus(){
+    if(!newEntry) display.innerText *= -1;
 }
